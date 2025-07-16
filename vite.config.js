403ec732e3492a76592fee8eb5df3133d5ea0529
KIRUBEL_TM <82014475+kirubel606@users.noreply.github.com>
2025-07-16 16:16:34 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,       // Allow access from network (bind to 0.0.0.0)
+    port: 3000,        // Change this to whatever port you want
+    strictPort: true,  // Optional: fail if port is already in use
   },
 })
